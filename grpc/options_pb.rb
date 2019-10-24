bd4a2c5c54d329ebdc39e45f0c065bb9ed3591c8
Ulderico Cirello <ulderico@strongdm.com>
2019-10-24 13:04:03 -0700

    add annotation to fields to indicate they should be exposed in the porcelain interface
    Addresses #4486
    
    GitOrigin-RevId: 78c3023048789cd6db9feb4a83cbc1becd6ea967

--- grpc/options_pb.rb
+++ grpc/options_pb.rb
@@ -8,6 +8,7 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
     add_message "v1.FieldOptions" do
       optional :name, :string, 1941300
       optional :sql_nullable, :bool, 1941301
+      optional :expose_as_porcelain, :bool, 1941302
     end
     add_message "v1.MessageOptions" do
       optional :model_name, :string, 1941300
--- grpc/v1_plumbing.rb
+++ grpc/v1_plumbing.rb
@@ -1,18 +1,18 @@
 require_relative './nodes_pb' # import GRPC plumbing
-require_relative '../models/nodes_porcelain' # import models porcelain
+require_relative '../models/v1_porcelain' # import models porcelain
 
-def Node_to_plumbing(porcelain)
+def node_to_plumbing(porcelain)
     plumbing = V1::Node.new()
     if porcelain .instance_of? Relay
         plumbing.relay = Relay_to_plumbing(porcelain)
     end
     if porcelain .instance_of? Gateway
         plumbing.gateway = Gateway_to_plumbing(porcelain)
-    end 
+    end
     plumbing
 end
 
-def Node_to_porcelain(plumbing)
+def node_to_porcelain(plumbing)
     if plumbing.relay != nil
         return Relay_to_porcelain(plumbing.relay)
     end
@@ -21,21 +21,39 @@ def Node_to_porcelain(plumbing)
     end
 end
 
-def Relay_to_porcelain(plumbing)
+def repeated_node_to_plumbing(porcelains)
+    items = Array.new
+    porcelains.each do |porcelain|
+        plumbing = node_to_plumbing(porcelain)
+        items.append(plumbing)
+    end
+    items
+end
+
+def relay_to_porcelain(plumbing)
     porcelain = Relay.new()
     porcelain.id = plumbing.id
     porcelain.name = plumbing.name
     porcelain
 end
 
-def Relay_to_plumbing(porcelain)
+def relay_to_plumbing(porcelain)
     plumbing = V1::Relay.new()
     plumbing.id = porcelain.id unless porcelain.id == nil
     plumbing.name = porcelain.name unless porcelain.name == nil
     plumbing
 end
 
-def Gateway_to_porcelain(plumbing)
+def repeated_relay_to_plumbing(porcelains)
+    items = Array.new
+    porcelains.each do |porcelain|
+        plumbing = relay_to_plumbing(porcelain)
+        items.append(plumbing)
+    end
+    items
+end
+
+def gateway_to_porcelain(plumbing)
     porcelain = Gateway.new()
     porcelain.id = plumbing.id
     porcelain.name = plumbing.name
@@ -44,12 +62,21 @@ def Gateway_to_porcelain(plumbing)
     porcelain
 end
 
-def Gateway_to_plumbing(porcelain)
+def gateway_to_plumbing(porcelain)
     plumbing = V1::Gateway.new()
     plumbing.id = porcelain.id unless porcelain.id == nil
     plumbing.name = porcelain.name unless porcelain.name == nil
     plumbing.listen_address = porcelain.listen_address unless porcelain.listen_address == nil
     plumbing.bind_address = porcelain.bind_address unless porcelain.bind_address == nil
     plumbing
+end
+
+def repeated_gateway_to_plumbing(porcelains)
+    items = Array.new
+    porcelains.each do |porcelain|
+        plumbing = gateway_to_plumbing(porcelain)
+        items.append(plumbing)
+    end
+    items
 end
 
