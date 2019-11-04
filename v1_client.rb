2355f42a8f7587852ea7196c4abbb9c6b903d53c
Evan Todd <evan@etodd.io>
2019-11-04 11:27:35 -0800

    use Ruby generator for iterable RPC output types
    Addresses #4486
    
    GitOrigin-RevId: a8c6dd009882580f915e724d1aa53be58709179f

--- v1_client.rb
+++ v1_client.rb
@@ -2,6 +2,7 @@ this_dir = File.expand_path(File.dirname(__FILE__))
 lib_dir = File.join(this_dir, 'grpc')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 require 'grpc'
+require 'enumerator'
 Dir[File.join(__dir__, 'grpc', '*.rb')].each {|file| require file }
 Dir[File.join(__dir__, 'models', '*.rb')].each {|file| require file }
 
@@ -24,6 +25,8 @@ module SDM
         
         # Create registers a new node.
         def create(nodes)
+            
+                
             req = V1::NodeCreateRequest.new()
             req.nodes += Plumbing::repeated_node_to_plumbing(nodes)
 
@@ -33,10 +36,13 @@ module SDM
             resp.nodes = Plumbing::repeated_node_to_porcelain(plumbing_response.nodes)
             resp.tokens = Plumbing::repeated_token_to_porcelain(plumbing_response.tokens)
             resp
+            
         end
         
         # Get reads one node by ID.
         def get(id)
+            
+                
             req = V1::NodeGetRequest.new()
             req.id = id
 
@@ -45,10 +51,13 @@ module SDM
             resp.meta = Plumbing::get_response_metadata_to_porcelain(plumbing_response.meta)
             resp.node = Plumbing::node_to_porcelain(plumbing_response.node)
             resp
+            
         end
         
         # Update patches a node by ID.
         def update(id, node)
+            
+                
             req = V1::NodeUpdateRequest.new()
             req.id = id
             req.node = Plumbing::node_to_plumbing(node)
@@ -58,10 +67,13 @@ module SDM
             resp.meta = Plumbing::update_response_metadata_to_porcelain(plumbing_response.meta)
             resp.node = Plumbing::node_to_porcelain(plumbing_response.node)
             resp
+            
         end
         
         # Delete removes a node by ID.
         def delete(id)
+            
+                
             req = V1::NodeDeleteRequest.new()
             req.id = id
 
@@ -69,21 +81,30 @@ module SDM
             resp = NodeDeleteResponse.new()
             resp.meta = Plumbing::delete_response_metadata_to_porcelain(plumbing_response.meta)
             resp
+            
         end
         
         # List is a batched Get call.
         def list(filter)
+            
+                
             req = V1::NodeListRequest.new()
             req.filter = filter
 
-            plumbing_response = @stub.list(req)
-            resp = NodeListResponse.new()
-            resp.nodes = Plumbing::repeated_node_to_porcelain(plumbing_response.nodes)
-            resp
+            g = Enumerator::Generator.new { |g|
+                plumbing_response = @stub.list(req)
+                plumbing_response.nodes.each do |plumbing_item|
+                    g.yield Plumbing::node_to_porcelain(plumbing_item)
+                end
+            }
+            g
+            
         end
         
         # BatchUpdate is a batched Update call.
         def batch_update(nodes)
+            
+                
             req = V1::NodeBatchUpdateRequest.new()
             req.nodes += Plumbing::repeated_node_to_plumbing(nodes)
 
@@ -92,10 +113,13 @@ module SDM
             resp.meta = Plumbing::batch_update_response_metadata_to_porcelain(plumbing_response.meta)
             resp.nodes = Plumbing::repeated_node_to_porcelain(plumbing_response.nodes)
             resp
+            
         end
         
         # BatchDelete is a batched Delete call.
         def batch_delete(ids)
+            
+                
             req = V1::NodeBatchDeleteRequest.new()
             req.ids += ids
 
@@ -103,6 +127,7 @@ module SDM
             resp = NodeBatchDeleteResponse.new()
             resp.meta = Plumbing::batch_delete_response_metadata_to_porcelain(plumbing_response.meta)
             resp
+            
         end
         
     end
