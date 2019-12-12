3c9bf383482979b0419ba44865cac8181a69841d
Patrick Stephen <patrick@strongdm.com>
2019-12-12 09:33:15 -0600

    add ruby role attachments test suite
    Addresses #4741
    
    GitOrigin-RevId: f58a38977f644e7427bbb1dc2d851b14409b1303

--- client.rb
+++ client.rb
@@ -3,51 +3,56 @@ require "base64"
 
 module SDM
 
-  # Client bundles all the services together and initializes them.
-  class Client
-    # Creates a new strongDM API client. The `address` parameter expects a hostname/port tuple.
-    def initialize(address, api_access_key, api_secret_key)
-      @api_access_key = api_access_key
-      @api_secret_key = Base64.strict_decode64(api_secret_key)
-      @nodes = Nodes.new(address, self)
-      @roles = Roles.new(address, self)
+    # Client bundles all the services together and initializes them.
+    class Client
+        # Creates a new strongDM API client. The `address` parameter expects a hostname/port tuple.
+        def initialize(address, api_access_key, api_secret_key)
+            @api_access_key = api_access_key
+            @api_secret_key = Base64.strict_decode64(api_secret_key)
+            @nodes = Nodes.new(address, self)
+            @role_attachments = RoleAttachments.new(address, self)
+            @roles = Roles.new(address, self)
 
-      @_test_options = Hash.new
+            @_test_options = Hash.new
+        end
+
+        def get_metadata(method_name, req)
+            return { 'x-sdm-authentication': @api_access_key,'x-sdm-signature': self.sign(method_name, req.to_proto)}
+        end
+
+        def sign(method_name, msg_bytes)
+            current_date = Time.new
+            date = sprintf("%04d-%02d-%02d",current_date.year, current_date.month, current_date.day)
+
+            signing_key = OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, @api_secret_key, date)
+            signing_key = OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, signing_key, 'sdm_api_v1')
+
+            sha_req = Digest::SHA256.new
+            sha_req << method_name
+            sha_req << "\n"
+            sha_req << msg_bytes
+            request_hash = sha_req.digest
+
+            return Base64.strict_encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, signing_key, request_hash))
+        end
+
+
+        # API authentication token (read-only).
+        attr_reader :api_access_key
+        # Nodes are proxies in the strongDM network. They come in two flavors: relays,
+        # which communicate with resources, and gateways, which communicate with
+        # clients.
+        attr_reader :nodes
+
+        attr_reader :role_attachments
+        # Roles are tools for controlling user access to resources. Each Role holds a
+        # list of resources which they grant access to. Composite roles are a special
+        # type of Role which have no resource associations of their own, but instead
+        # grant access to the combined resources associated with a set of child roles.
+        # Each user can be a member of one Role or composite role.
+        attr_reader :roles
+
+        attr_reader :_test_options
     end
 
-    def get_metadata(method_name, req)
-      return { 'x-sdm-authentication': @api_access_key, 'x-sdm-signature': self.sign(method_name, req.to_proto) }
-    end
-
-    def sign(method_name, msg_bytes)
-      current_date = Time.new
-      date = sprintf("%04d-%02d-%02d", current_date.year, current_date.month, current_date.day)
-
-      signing_key = OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, @api_secret_key, date)
-      signing_key = OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, signing_key, "sdm_api_v1")
-
-      sha_req = Digest::SHA256.new
-      sha_req << method_name
-      sha_req << "\n"
-      sha_req << msg_bytes
-      request_hash = sha_req.digest
-
-      return Base64.strict_encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, signing_key, request_hash))
-    end
-
-    # API authentication token (read-only).
-    attr_reader :api_access_key
-    # Nodes are proxies in the strongDM network. They come in two flavors: relays,
-    # which communicate with resources, and gateways, which communicate with
-    # clients.
-    attr_reader :nodes
-    # Roles are tools for controlling user access to resources. Each Role holds a
-    # list of resources which they grant access to. Composite roles are a special
-    # type of Role which have no resource associations of their own, but instead
-    # grant access to the combined resources associated with a set of child roles.
-    # Each user can be a member of one Role or composite role.
-    attr_reader :roles
-
-    attr_reader :_test_options
-  end
 end
--- errors/errors.rb
+++ errors/errors.rb
@@ -1,71 +1,69 @@
 module SDM
 
-  # RPCError is a generic RPC error
-  class RPCError < StandardError
-    attr_reader :code
-
-    def initialize(msg, code)
-      @code = code
-      super(msg)
+    # RPCError is a generic RPC error
+    class RPCError < StandardError
+        attr_reader :code
+        def initialize(msg, code)
+            @code = code
+            super(msg)
+        end
     end
-  end
 
-  # DeadlineExceededError indicates an RPC call timed out
-  class DeadlineExceededError < RPCError
-    def initialize(msg)
-      super(msg, 4)
+    # DeadlineExceededError indicates an RPC call timed out
+    class DeadlineExceededError < RPCError
+        def initialize(msg)
+            super(msg, 4)
+        end
     end
-  end
 
-  # AlreadyExistsError is used when an entity already exists in the system
-  class AlreadyExistsError < RPCError
-    def initialize(msg)
-      super(msg, 6)
+    # AlreadyExistsError is used when an entity already exists in the system
+    class AlreadyExistsError < RPCError
+        def initialize(msg)
+            super(msg, 6)
+        end
     end
-  end
 
-  # NotFoundError is used when an entity does not exist in the system
-  class NotFoundError < RPCError
-    def initialize(msg)
-      super(msg, 5)
+    # NotFoundError is used when an entity does not exist in the system
+    class NotFoundError < RPCError
+        def initialize(msg)
+            super(msg, 5)
+        end
     end
-  end
 
-  # BadRequestError identifies a bad request sent by the client
-  class BadRequestError < RPCError
-    def initialize(msg)
-      super(msg, 3)
+    # BadRequestError identifies a bad request sent by the client
+    class BadRequestError < RPCError
+        def initialize(msg)
+            super(msg, 3)
+        end
     end
-  end
 
-  # AuthenticationError is used to specify an authentication failure condition
-  class AuthenticationError < RPCError
-    def initialize(msg)
-      super(msg, 16)
+    # AuthenticationError is used to specify an authentication failure condition
+    class AuthenticationError < RPCError
+        def initialize(msg)
+            super(msg, 16)
+        end
     end
-  end
 
-  # PermissionError is used to specify a permissions violation
-  class PermissionError < RPCError
-    def initialize(msg)
-      super(msg, 7)
+    # PermissionError is used to specify a permissions violation
+    class PermissionError < RPCError
+        def initialize(msg)
+            super(msg, 7)
+        end
     end
-  end
 
-  # InternalError is used to specify an internal system error
-  class InternalError < RPCError
-    def initialize(msg)
-      super(msg, 13)
+    # InternalError is used to specify an internal system error
+    class InternalError < RPCError
+        def initialize(msg)
+            super(msg, 13)
+        end
     end
-  end
 
-  # RateLimitError is used for rate limit excess condition
-  class RateLimitError < RPCError
-    attr_reader :rate_limit
-
-    def initialize(msg, rate_limit)
-      @rate_limit = rate_limit
-      super(msg, 8)
+    # RateLimitError is used for rate limit excess condition
+    class RateLimitError < RPCError
+        attr_reader :rate_limit
+        def initialize(msg, rate_limit)
+            @rate_limit = rate_limit
+            super(msg, 8)
+        end
     end
-  end
 end
