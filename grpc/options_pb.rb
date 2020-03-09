5e592aa155f6813c05064edfed1bc9a0c6a0a660
Ulderico Cirello <uldericofilho@gmail.com>
2020-03-09 13:05:14 -0700

    Merge commit
    GitOrigin-RevId: 7efbb7e5d7809f7376b6cd4e7de9b9fe1672ef45

--- grpc/options_pb.rb
+++ grpc/options_pb.rb
@@ -28,6 +28,7 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
       optional :id_type, :string, 1941305
       optional :sdk_only, :bool, 1941306
       optional :computed, :bool, 1941307
+      optional :force_new, :bool, 1941308
     end
     add_message "v1.MessageOptions" do
       optional :model_name, :string, 1941300
