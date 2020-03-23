3aefe8f6a389cd8769703580037f3d908d350d3a
Ulderico Cirello <uldericofilho@gmail.com>
2020-03-23 13:23:03 -0700

    ruby/examples/okta-sync: filter by the given search criteria
    Addresses #5512
    
    GitOrigin-RevId: 777dd079daba10354e282f08f1dc249458002deb

--- examples/okta-sync/oktaSync.rb
+++ examples/okta-sync/oktaSync.rb
@@ -2,7 +2,6 @@ require 'yaml'
 require 'strongdm'
 require 'oktakit'
 
-
 SDM_API_ACCESS_KEY = ENV.fetch('SDM_API_ACCESS_KEY', '')
 SDM_API_SECRET_KEY = ENV.fetch('SDM_API_SECRET_KEY','')
 OKTA_CLIENT_TOKEN = ENV.fetch('OKTA_CLIENT_TOKEN', '')
@@ -20,13 +19,14 @@ okta_users = Array.new()
 okta_client = Oktakit.new(token: OKTA_CLIENT_TOKEN, api_endpoint: OKTA_CLIENT_ORGURL+"/api/v1")
 
 options = {
-	'search':
-	"profile.department eq \"Engineering\" and (status eq \"ACTIVE\")"
+	'query':{
+		'search': "profile.department eq \"Engineering\" and (status eq \"ACTIVE\")"
+	}
 }
-	
-all_users = okta_client.list_users(options:options)
 
-all_users[0].each{ |u| 
+all_users = okta_client.list_users(options)
+
+all_users[0].each{ |u|
 	groups = okta_client.get_member_groups(u.id)
 	group_names = Array.new()
 	groups[0].each{ |ug|
@@ -46,13 +46,15 @@ grants.each{ |g|
 }
 
 desired = {}
+overlapping = 0
 matchers["groups"].each{ |group|
 	group["resources"].each { |resourceQuery|
 		client.resources.list(resourceQuery).each { |res|
 			okta_users.each { |u|
 				if u[:groups].include? group["name"]
 					aid = accounts[u[:login]]
-					if aid != nil 
+					if aid != nil
+						overlapping += 1
 						desired[aid] = [] if not desired[aid]
 						desired[aid].push(res.id)
 					end
@@ -62,27 +64,31 @@ matchers["groups"].each{ |group|
 	}
 }
 
+revocations = 0
 current.each { |aid, curRes|
 	desRes = desired[aid]
 	desRes = [] if not desired[aid]
 	curRes.each { |r|
 		if not (desRes.include? r[:resource_id])
-			puts "revoking #{r[:resource_id]} from #{aid} (#{r[:id]})"
 			client.account_grants.delete(r[:id])
+			revocations += 1
 		end
 	}
 }
 
+grants = 0
 desired.each { |aid, desRes|
 	curRes = current[aid]
 	curRes = [] if not current[aid]
 	desRes.each { |r|
 		if not (curRes.map{|c| c[:resource_id]}.include? r)
-			puts "granting #{r} to #{aid}"
 			ag = SDM::AccountGrant.new()
 			ag.account_id = aid
 			ag.resource_id = r
 			client.account_grants.create(ag)
+			grants +=1
 		end
 	}
 }
+
+puts "%d Okta users, %d strongDM users, %d overlapping users, %d grants, %d revocations" % [okta_users.size, accounts.size, overlapping, grants, revocations]
