681aa16bbec4b06092f139d95e8a6fee8a1a660b
Ulderico Cirello <uldericofilho@gmail.com>
2020-03-23 14:58:29 -0700

    ruby: remove duplicated license headers
    Addresses #5512
    
    GitOrigin-RevId: dcad681801022bdef1e9f7f375e80f31ec85f008

--- examples/listUsers.rb
+++ examples/listUsers.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 require "strongdm"
 
 client = SDM::Client.new(ENV["SDM_API_ACCESS_KEY"], ENV["SDM_API_SECRET_KEY"])
--- examples/okta-sync/oktaSync.rb
+++ examples/okta-sync/oktaSync.rb
@@ -11,9 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 require "yaml"
-require 'strongdm'
+require "strongdm"
 require "oktakit"
 require "optparse"
 
