f811872cb05c2f4e90dd30adf1d6fd9e5f837211
Ulderico Cirello <uldericofilho@gmail.com>
2020-03-23 15:07:15 -0700

    ruby: bump version
    Addresses #5512
    
    GitOrigin-RevId: 0f80767a72ac334f71af4967bc0fa1b35403ec80

--- examples/listUsers.rb
+++ examples/listUsers.rb
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 require "strongdm"
 
 client = SDM::Client.new(ENV["SDM_API_ACCESS_KEY"], ENV["SDM_API_SECRET_KEY"])
