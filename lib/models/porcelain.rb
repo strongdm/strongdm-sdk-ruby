d3ba64b9e0147a8f79b33775f1ca635dbcad3b68
SupportSDM <support@strongdm.com>
2025-01-14 21:13:54 +0000

    12.2.0

--- lib/models/porcelain.rb
+++ lib/models/porcelain.rb
@@ -5207,7 +5207,6 @@ module SDM
     end
   end
 
-  # GCPConsole is currently unstable, and its API may change, or it may be removed, without a major version bump.
   class GCPConsole
     # The bind interface is the IP address to which the port override of a resource is bound (for example, 127.0.0.1). It is automatically generated if not provided.
     attr_accessor :bind_interface
@@ -5314,7 +5313,6 @@ module SDM
     end
   end
 
-  # GCPWIF is currently unstable, and its API may change, or it may be removed, without a major version bump.
   class GCPWIF
     # The bind interface is the IP address to which the port override of a resource is bound (for example, 127.0.0.1). It is automatically generated if not provided.
     attr_accessor :bind_interface
--- lib/version.rb
+++ lib/version.rb
@@ -13,5 +13,5 @@
 # limitations under the License.
 #
 module SDM
-  VERSION = "12.1.0"
+  VERSION = "12.2.0"
 end
