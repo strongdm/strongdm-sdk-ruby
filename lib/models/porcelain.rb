2644f38f94173025b6b84a36d78e841aa3e4cdf0
SupportSDM <support@strongdm.com>
2025-06-20 13:14:58 +0000

    14.25.0

--- lib/models/porcelain.rb
+++ lib/models/porcelain.rb
@@ -14828,7 +14828,6 @@ module SDM
     end
   end
 
-  # Trino is currently unstable, and its API may change, or it may be removed, without a major version bump.
   class Trino
     # The bind interface is the IP address to which the port override of a resource is bound (for example, 127.0.0.1). It is automatically generated if not provided.
     attr_accessor :bind_interface
--- lib/version.rb
+++ lib/version.rb
@@ -13,5 +13,5 @@
 # limitations under the License.
 #
 module SDM
-  VERSION = "14.24.0"
+  VERSION = "14.25.0"
 end
